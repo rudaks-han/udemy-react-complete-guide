124aca98eebc2221f5b7f2861880ddc9ed319f31
한경만 <kmhan@spectra.co.kr>
2023-11-30 07:54:40 +0900

    update

---
+++ food-order-project/src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,22 @@
+import classes from './MeailItemForm.module.css';
+import Input from '../../UI/Input';
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amout"
+        input={{
+          id: 'amount',
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1',
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
