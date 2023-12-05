f4118668f7252cf4003ae1755e6bedf098494b33
rudaks <rudaks94@gmail.com>
2023-12-05 19:55:20 +0900

    useCallback 사용법

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,4 +1,4 @@
-import {useMemo, useState} from "react";
+import {useCallback, useState} from "react";
 import Child from "./Child";
 
 function App() {
@@ -10,11 +10,8 @@ function App() {
 
   console.log('부모 컴포넌트 변경');
 
-  const name = useMemo(() => {
-    return {
-        lastName: '홍',
-        firstName: '길동'
-    }
+  const tellMe = useCallback(() => {
+      console.log('길동아 사랑해');
   }, []);
 
   return (
@@ -22,7 +19,7 @@ function App() {
       <h1>부모</h1>
       <p>age: {parentAge}</p>
       <button onClick={incrementParentAge}>부모 나이 증가</button>
-      <Child name={name} />
+      <Child name={'홍길동'} tellMe={tellMe}/>
     </div>
   );
 }
